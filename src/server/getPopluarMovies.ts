d124627e2a31e72f149f45a3bc6955106b6ddd5e
jaemin <lemon__96@naver.com>
2024-07-08 20:58:10 +0900

    fix: update API - getPopularMovies
    로직 일부 수정 - then 구문에서 변수 정의 형으로
    체이닝 방식에서 각 단계별로 관리하는 방식으로 개선됨
    
    Github issue #11

--- src/server/getPopluarMovies.ts
+++ src/server/getPopluarMovies.ts
@@ -5,21 +5,11 @@ const { TMDB_API_KEY } = ServerConfig;
 
 const TMDB_API_URI = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_API_KEY || ""}`;
 
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-  }
-};
-
 const getPopularMovies = async () => {
     try {
-      const data = await fetch(TMDB_API_URI, options)
-        .then(res => res.json())
-        .then(json => console.log(json))
-        .catch(err => console.error('error:' + err));
-
-        return data;
+      const response = await fetch(TMDB_API_URI);
+      const data = await response.json();
+      return data; 
     }catch(error) {
         console.error(error);
     };
