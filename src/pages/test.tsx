4b2da03be9a945b554f41e020f97c0695b72d108
jaemin <lemon__96@naver.com>
2024-07-08 20:59:24 +0900

    feat: Mockup 테스트 코드 추가
    Github issue #11

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,9 +1,18 @@
-import React, { ReactElement } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button } from '@mui/material';
 import { RootState } from '../redux/modules';
 import { QuestionForm, Spinner } from '../components';
 import { Increase, Decrease } from '../redux/modules/counter';
+
+import {
+  useQuery,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { getPopularMovies } from '../server';
+
+const queryClient = new QueryClient();
 
 const TestPage = (): ReactElement => {
   const dispatch = useDispatch();
@@ -27,8 +36,44 @@ const TestPage = (): ReactElement => {
       <div>
         <Spinner />
       </div>
+      <QueryClientProvider client={queryClient}>
+        <MockUp />
+      </QueryClientProvider>
     </div>
   );
 };
 
 export default TestPage;
+
+
+// mock-up date test
+const MockUp = () => {
+  const [movies, setMovies] = useState();
+  const { isLoading, isError, data } = useQuery({ queryKey: ['popularMovies'], queryFn: getPopularMovies });
+
+  useEffect(() => {
+    if(data) {
+      setMovies(data);
+    }
+  }, [data]);
+
+  const handleMovies = () => {
+    console.log({movies});
+  }
+
+  if(isError) {
+    return <>Error!</>
+  }
+
+  if(isLoading) {
+    return <Spinner />
+  }
+
+  return (
+    <>
+      <Button onClick={handleMovies}>
+        인기 영화 확인하기
+      </Button>
+    </>
+  )
+}
