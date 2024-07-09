04127d42177afb479db6d46cdb459eb037e0372e
jaemin <lemon__96@naver.com>
2024-07-09 20:32:26 +0900

    refactor: useQuery 사용 로직 개선
    custom hook 을 통해 관리되는 영화 데이터를 사용하도록 변경
    원하는 곳에서 데이터만 가져와 사용할 수 있도록 개선

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,22 +1,19 @@
-import { ReactElement, useEffect, useState } from 'react';
+import { ReactElement } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button } from '@mui/material';
 import { RootState } from '../redux/modules';
 import { QuestionForm, Spinner } from '../components';
 import { Increase, Decrease } from '../redux/modules/counter';
-
-import {
-  useQuery,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
-import { getPopularMovies } from '../server';
-
-const queryClient = new QueryClient();
+import { useMovie } from '../hooks';
 
 const TestPage = (): ReactElement => {
   const dispatch = useDispatch();
   const { num } = useSelector((state: RootState) => state.counter);
+  const { data } = useMovie();
+
+  const handleMovie = () => {
+    console.log({data});
+  }
 
   return (
     <div>
@@ -36,44 +33,9 @@ const TestPage = (): ReactElement => {
       <div>
         <Spinner />
       </div>
-      <QueryClientProvider client={queryClient}>
-        <MockUp />
-      </QueryClientProvider>
+      <Button onClick={handleMovie}>인기 영화 보기</Button>
     </div>
   );
 };
 
 export default TestPage;
-
-
-// mock-up date test
-const MockUp = () => {
-  const [movies, setMovies] = useState();
-  const { isLoading, isError, data } = useQuery({ queryKey: ['popularMovies'], queryFn: getPopularMovies });
-
-  useEffect(() => {
-    if(data) {
-      setMovies(data);
-    }
-  }, [data]);
-
-  const handleMovies = () => {
-    console.log({movies});
-  }
-
-  if(isError) {
-    return <>Error!</>
-  }
-
-  if(isLoading) {
-    return <Spinner />
-  }
-
-  return (
-    <>
-      <Button onClick={handleMovies}>
-        인기 영화 확인하기
-      </Button>
-    </>
-  )
-}
